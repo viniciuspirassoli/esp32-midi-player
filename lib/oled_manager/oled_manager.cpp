a147aad38cd901fa4ea9629b86af6054f8b7f15e
moraless <moraless93@icloud.com>
2023-11-27 18:17:19 +0000

    Addind scrollSongName and displayPauseIcon methods

--- lib/oled_manager/oled_manager.cpp
+++ lib/oled_manager/oled_manager.cpp
@@ -8,10 +8,29 @@ void OLEDManager::begin(const int sda, const int scl) {
 
 }
 
-void OLEDManager::displaySongName(const char* songName)
-{
-  oled.setFont(u8x8_font_amstrad_cpc_extended_f);
+void OLEDManager::displaySongName(const String songName){
   oled.clear();
-  oled.setCursor(0, 0);
+  int screenWidth = oled.getCols();
+  int textWidth = oled.getUTF8Len(songName.c_str());
+  oled.setCursor((screenWidth - textWidth) / 2, oled.getRows() / 2);
   oled.print(songName);
+}
+
+void OLEDManager::scrollSongName(String songName, const unsigned short delayTime){
+  oled.clear();
+  int totalColums = oled.getCols();
+  for (int i=0; i < oled.getCols(); i++) {
+    songName = " " + songName;  
+  } 
+  songName = " " + songName; 
+  for (int position = 0; position < songName.length(); position++) {
+    oled.setCursor(0, oled.getRows() / 2);
+    oled.print(songName.substring(position, position + oled.getCols()));
+    delay(delayTime);
+  }
+}
+
+void OLEDManager::displayPauseIcon(){
+  oled.setCursor((oled.getCols() - 3) / 2, oled.getRows() / 2 + 1);
+  oled.print("||");
 }
--- lib/oled_manager/oled_manager.h
+++ lib/oled_manager/oled_manager.h
@@ -8,7 +8,9 @@
 class OLEDManager {
 public:
     void begin(const int sda, const int scl);
-    void displaySongName(const char* songName);
+    void displaySongName(const String songName);
+    void scrollSongName(String songName, const unsigned short delayTime);
+    void displayPauseIcon();
 
 private:
     U8X8_SSD1306_128X64_NONAME_HW_I2C oled /*(U8X8_PIN_NONE)*/; 
